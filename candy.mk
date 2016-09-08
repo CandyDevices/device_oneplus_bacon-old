345029a4f6a6f48a7b4b46ef2b17aaea6e7dd034
Dan Cartier <nospamdan1@gmail.com>
2016-09-07 23:40:35 -0400

    bacon: Don't build NFC-enhanced for now
    Change-Id: Ibf01f4293d313f14ebe1447eebb8777de2a7e465

--- candy.mk
+++ candy.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/bacon/bacon.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/candy/config/nfc_enhanced.mk)
+#$(call inherit-product, vendor/candy/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/candy/config/common_full_phone.mk)
