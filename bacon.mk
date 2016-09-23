dfb6f54817b51fadbf15911222c6306ccdd92cbb
Dan Cartier <nospamdan1@gmail.com>
2016-09-23 04:36:00 +0200

    Revert "bacon: Don't include oppo-common for now"
    This reverts commit 030f31c42f82f5936f30a852fa48b324629e8066.
    
    Change-Id: I2d95f5399792cfeead0a7a96d8612e07da6b44a6

--- bacon.mk
+++ bacon.mk
@@ -251,4 +251,4 @@ PRODUCT_PACKAGES += \
     OnePlusDoze
 
 # Inherit from oppo-common
-#$(call inherit-product, device/oppo/common/common.mk)
+$(call inherit-product, device/oppo/common/common.mk)
